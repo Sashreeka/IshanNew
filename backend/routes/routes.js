6fc01aade723d4ef3576a8aa1580f033f010cea0
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-14 12:03:02 +0530

    add database schema,and routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,10 +1,23 @@
-const { request } = require('express')
 const express=require('express')
 const router=express.Router()
+const signUpTemplateCopy=require('../models/SignUpModel');
 
 //signup page
 router.post('/signup',(request,response)=>{
-    response.send('send')
+  const signedUpUser=new signUpTemplateCopy({
+      fullName:request.body.fullName,
+      username:request.body.username,
+      email:request.body.email,
+      password:request.body.password
+
+  })
+  signedUpUser.save()
+  .then(data=>{
+      response.json(data)
+  })
+  .catch(error=>{
+      response.json(error)
+  })
 })
 
 module.exports=router
