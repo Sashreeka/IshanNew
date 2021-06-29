3ca736eaee98366e0e0788083864bdcc5e9ca642
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-29 18:56:53 +0530

    add alert box

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, FlatList,Image } from 'react-native';
+import { StyleSheet, Text, View, FlatList,Image,TouchableOpacity,Alert } from 'react-native';
+
 
 export default function App() {
 
@@ -13,6 +14,24 @@ export default function App() {
     {id:"6",name:'ishan6'},
 
   ])
+
+  const alertCreate= ()=>{
+    Alert.alert(
+      'Alert Box',
+      'Do you want to sure',
+      [
+        {
+          text: 'cancel',
+          onPress: ()=> console.log('cancel')
+        },
+        {
+          text: 'ok',
+          onPress: ()=>console.log('ok')
+        }
+      ]
+    );
+
+  }
   return (
     <View style={styles.container}>
     <FlatList
@@ -22,15 +41,19 @@ export default function App() {
       keyExtractor={(item) => item.id}
      
       renderItem= {({item})=>(
+
+        <TouchableOpacity onPress={alertCreate}>
+
         <View key={item.id}
         style={styles.horizontal}>
           <Image
           style={styles.img}
          
-            source={require('./assets/icon.png')}
+            source={require('./assets/d.png')}
           />
           <Text >{item.name}</Text>
         </View>
+        </TouchableOpacity>
       )}
     />
       
@@ -54,8 +77,8 @@ const styles = StyleSheet.create({
 
   },
   img:{
-    width:10,
-    height:10,
+    width:50,
+    height:50,
 
   }
 });
