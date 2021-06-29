e41528aec4ff345b18ae8f61cf0379f1cadc0798
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-29 13:10:28 +0530

    add project

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [data,setData]=useState([
+    {id:1,name:'ishan'},
+    {id:2,name:'ishan2'},
+    {id:3,name:'ishan3'},
+    {id:4,name:'ishan4'},
+    {id:5,name:'ishan5'},
+    {id:6,name:'ishan6'},
+    {id:7,name:'ishan7'},
+    {id:8,name:'ishan8'},
+  ])
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <FlatList
+    
+      data={data}
+      horizontal={true}
+      renderItem= {({item})=>(
+        <Text style={styles.horizontal}>{item.name}</Text>
+      )}
+    />
+      
     </View>
   );
 }
@@ -18,4 +36,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  horizontal:{
+    width:100,
+    marginTop:40,
+    marginLeft:20,
+    height:40,
+    backgroundColor:'teal'
+
+  }
 });
