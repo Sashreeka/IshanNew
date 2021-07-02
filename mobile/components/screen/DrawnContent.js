897082dff655eed23ef6401a86312ece97fd6ff3
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-07-02 21:20:13 +0530

    add style in drawer

--- mobile/components/screen/DrawnContent.js
+++ mobile/components/screen/DrawnContent.js
@@ -1,19 +1,23 @@
 import React,{useState,useEffect} from 'react';
 import { StyleSheet, 
-    Text, 
+   
     View, 
-    FlatList,
-    Image,
-    TouchableOpacity,
-    Alert,
-    Modal,
-    Button 
+   
   } from 'react-native';
   import {
       DrawerContentScrollView,
       DrawerItem
   } from '@react-navigation/drawer';
-
+  import {  
+    Avatar, 
+    Title, 
+    Caption,
+    Paragraph,
+    Drawer,
+    Text,
+    TouchableOpacity,
+    Switch 
+  } from 'react-native-paper';
 
   export function DrawerContent(props)
   {
@@ -23,3 +27,52 @@ import { StyleSheet,
           </View>
       );
   }
+
+  const styles=StyleSheet.create({
+      drawerContent:{
+          flex:1,
+      },
+      userInfoSelection:{
+
+        paddingLeft:20,
+      },
+      title:{
+          fontSize:14,
+          marginTop:3,
+          fontWeight:"bold",
+      },
+      caption:{
+          fontSize:14,
+          lineHeight:14,
+      },
+      row:{
+          marginTop:20,
+          flexDirection:'row',
+          alignItems:'center',
+      },
+      section:{
+          flexDirection:'row',
+          alignItems:'center',
+          marginRight:15,
+      },
+      paragarph:{
+          fontWeight:'bold',
+          marginRight:3,
+
+      },
+      drawerSection:{
+          marginTop:15,
+      },
+      bottomDrawerSection:{
+          marginBottom:15,
+          borderTopColor:'#f4f4f4',
+          borderTopWidth:1,
+      },
+      preference:{
+          flexDirection:'row',
+          justifyContent:'space-between',
+          paddingVertical:12,
+          paddingHorizontal:16,
+      }
+
+  })
