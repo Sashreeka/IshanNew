da0fbbcfda883818b9abe587b6966406aa6e8eda
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-07-02 23:48:59 +0530

    signin, signup template add

---
+++ mobile/components/screen/RootStackScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SplashScreen from './SplashScreen';
+import SigninScreen from './SigninScreen';
+import SignupScreen from './SignupScreen';
+
+
+const RootStack=createStackNavigator();
+export default function RootStackScreen({navigation}) {
+    return (
+        <RootStack.Navigator headerMode="none">
+            <RootStack.Screen name="SplashScreen" component={SplashScreen}/>
+            <RootStack.Screen name="SigninScreen" component={SigninScreen}/>
+            <RootStack.Screen name="SignupScreen" component={SignupScreen}/>
+
+        </RootStack.Navigator>
+    )
+}
