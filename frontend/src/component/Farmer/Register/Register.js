425a06a9085d58a8e77299a621a59a416f75a294
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-07-06 02:59:52 +0530

    add validation using Yup

--- frontend/src/component/Farmer/Register/Register.js
+++ frontend/src/component/Farmer/Register/Register.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import {Formik,Form} from 'formik';
 import TextField from './TextField';
+import * as Yup from 'yup';
 
 export default function Register() {
+
+    const validate= Yup.object({
+        name: Yup.string()
+            .max(15,'Must be 15 characters or less')
+            .required('Name is  required'),
+        telephone: Yup.string()
+            .min(10,'Telephone number is invalid')
+            .max(10,'Telephone number is invalid')
+            .required('Telephone is required'),
+        email: Yup.string()
+            .email('Email is invalid')
+            .required('Email is required'),
+        password: Yup.string()
+            .min(6,'Password must be at least 6 characters')
+            .required('Password is required'),
+        confirmPassword: Yup.string()
+            .oneOf([Yup.ref('password'),null],'Password must match')
+            .required('Confirm Password is required'), 
+
+    })
+
+
     return (
         <Formik
         initialValues={{
@@ -12,7 +35,9 @@ export default function Register() {
             password:'',
             confirmPassword:''
 
-        }}>
+        }}
+        validationSchema={validate}
+        >
            {
                formik =>(
                    <div>
