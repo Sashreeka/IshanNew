ed54346df1498e226ccdab5d633b6661eabc7ba9
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-07-06 01:55:58 +0530

    add text field seperatedly

--- frontend/src/component/Farmer/Register/Register.js
+++ frontend/src/component/Farmer/Register/Register.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import {Formik,Form} from 'formik';
+import TextField from './TextField';
 
 export default function Register() {
     return (
-        <Formik>
+        <Formik
+        initialValues={{
+            name:'',
+            telephone:'',
+            email:'',
+            password:'',
+            confirmPassword:''
+
+        }}>
            {
-               formik => {
-                   console.log(formik)
-               }
+               formik =>(
+                   <div>
+                       <h1 className="my-4 font-weight-bold-display-4">Sign Up</h1>
+                       {console.log(formik)}
+                       <Form>
+                            <TextField/>
+
+                       </Form>
+                   </div>
+               )
            }
             
         </Formik>
---
+++ frontend/src/component/Farmer/Register/TextField.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TextField() {
+    return (
+        <div>
+            <input
+                type="text"
+            />
+            
+        </div>
+    )
+}
