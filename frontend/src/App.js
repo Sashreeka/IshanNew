1c3530daf585b648facec81d8f32719516550046
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-07-05 19:42:05 +0530

    create register file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import "./component/Style/style.css"
 
 export default function App() {
   return (
-    <div>
+    <div className="App">
+    <h1>Registration Form</h1>
+    <div className="form">
+    <label>Enter Name</label>
+    <input type="text" name="name" placeholder="ishan reshmika"/>
+
+    <label>Enter Telephone</label>
+    <input type="text" name="telephone" placeholder="0768610084"/>
+
+    <label>Enter Email</label>
+    <input type="text" name="email" placeholder="ishan@gmail.com"/>
+
+    <button>Submit</button>
+
+
+    </div>
       
     </div>
   )
